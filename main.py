f383bb416a24d7bc39de8c2ec914fab7acf58cf6
AntonMilienkov <84272195+AntonMilienkov@users.noreply.github.com>
2023-09-17 23:45:29 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from prometheus_client import Gauge, start_http_server
 import time
 
-# Задаем параметры через экземпляр Options, который передадим веб-драйверу
 chrome_options = Options()
 chrome_options.add_argument('--headless')
 
@@ -30,21 +30,24 @@ function refreshLoop() {
 refreshLoop();
 return fps;
 """
+var FPS = prometheus.NewGauge(
+   prometheus.GaugeOpts{
+       Name: "average FPS",
+       Help: "amount of rendering frames per second",
+   },
+)
 
 try:
-    duration_seconds = 10   # время в секундах, в течении которого будет работать скрипт
-
-    # переходим на страничку гугл.ком, пишем в поисковую строку Example search и нажимаем ентер
+    duration_seconds = 10   
     driver.get('https://google.com')
     textarea = driver.find_element(By.CSS_SELECTOR, 'textarea')
     textarea.send_keys('Example search')
-    textarea.send_keys(Keys.RETURN)  # Press Enter
+    textarea.send_keys(Keys.RETURN) 
 
     start_time = time.time()
     fps_values = []
 
     while time.time() - start_time < duration_seconds:
-        # Симулируем скролл страницы результатов поиска и запускаем скрипт из переменной js_script
         driver.find_element(By.CSS_SELECTOR, 'body').send_keys(Keys.ARROW_DOWN)
         fps = driver.execute_script(js_script)
         print(fps)
@@ -52,6 +55,7 @@ try:
         time.sleep(1)
 
     average_fps = sum(fps_values) / len(fps_values)
+    g.set(average_fps)
     print(f'Средний ФПС за {duration_seconds} секунд: {average_fps:.2f}')
 except Exception as e:
     print(f"Ошибка: {str(e)}")
